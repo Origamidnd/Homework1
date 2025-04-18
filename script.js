532fa5b5fcc4ef0c2ae946134fbb183a9444f07f
Origamidnd <origamidnd@gmail.com>
2025-04-19 01:10:35 +0300

    create new branch

---
+++ script.js
@@ -0,0 +1,84 @@
+// Задание 1
+let a = 10;
+alert(a);
+
+a = 20;
+alert(a);
+
+
+// Задание 2
+const iphoneYear = 2007;
+alert(iphoneYear);
+
+
+//Задание 3
+const jsCreate = "Брендан Эйх";
+alert(jsCreate);
+
+//Задание 4
+let ten = 10;
+let two = 2;
+
+alert(ten + two);
+alert(ten - two);
+alert(ten / two);
+alert(ten * two);
+alert(ten % two);
+
+
+//Задание 5
+let degree = 2;
+
+alert(degree ** 5);
+
+
+// Задание 6
+let a = 9;
+let b = 2;
+
+alert(a % b);
+
+
+// Задание 7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+// Задание 8
+let age = Number(prompt("Сколько Вам лет?"));
+alert(age);
+
+
+// Задание 9
+let person = {
+    name: "Глеб",
+    age: 32,
+    isAdmin: true
+};
+
+
+// Здание 10
+
+let name = prompt("Как Вас зовут?");
+alert(`Привет, ${name}!`);
+
+
+// Задание 11
+let num = Number(prompt("Введите любое число число"));
+
+let doubleNum = num * 2;
+alert(doubleNum);
+
+let plusTen = doubleNum + 10;
+alert(plusTen);
+
+let divideTwo = plusTen / 2;
+alert(divideTwo);
+
+let result = divideTwo - num;
+alert(result);
